d8d1544d9b5c7e56c0bf205e0d0d0b514e5ca9b7
Epi0702 <rnqhsrhkd@gmail.com>
2024-05-24 15:31:49 +0900

    반응형으로 제작중
    반응형이 뭘까

---
+++ Hodu_HalfYesBrother/gridResizer.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const grid = document.querySelector('.photoAlbumGridContainer');
+
+    function updateGridGap()
+    {
+        //grid 스타일 갖고오기
+        const computedStyle = window.getComputedStyle(grid);
+        //column-gap value (%로 되어있어서 px로 변환해야함)
+        const gridColumnGap  = computedStyle.getPropertyValue('column-gap');
+        //부모 가로
+        const parentWidth = grid.clientWidth;
+        //계산
+        let percentageValue = parseFloat(gridColumnGap) / 100;
+        let gapAsPx = parentWidth * percentageValue + 'px';
+
+        if(gapAsPx > '32' + 'px') {
+            grid.style.rowGap = gapAsPx;
+        }
+        else
+        {
+            grid.style.rowGap = 32 + 'px';
+        }
+
+        console.log(grid.style.rowGap)
+        console.log("Grid Gap Updated!!")
+    }
+
+    updateGridGap();
+    window.addEventListener('resize', updateGridGap);
+});
---
+++ Hodu_HalfYesBrother/kakaoMap.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    kakao.maps.load(function () {
+        var mapContainer = document.getElementById('map') // 지도를 표시할 div
+        var mapOption = {
+            center: new kakao.maps.LatLng(37.49456189702631, 126.87209080539368), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
+        // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+        var map = new kakao.maps.Map(mapContainer, mapOption);
+
+        // 지도를 클릭한 위치에 표출할 마커입니다
+        var marker = new kakao.maps.Marker({
+            // 지도 중심좌표에 마커를 생성합니다
+            position: map.getCenter()
+        });
+// 지도에 마커를 표시합니다
+        marker.setMap(map);
+
+        function setDraggable(draggable)
+        {
+            map.setDraggable(draggable);
+        }
+
+        setDraggable(false);
+
+        function resizeMap() {
+
+            map.relayout();
+            map.setCenter(mapOption.center);
+            console.log("Map Resized!!");
+        }
+
+        window.addEventListener('resize', resizeMap);
+    })
+})
+
+
