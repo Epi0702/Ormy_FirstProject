fcf191150c6cbadc78a3b507040ae8bec5a45ec8
Epi0702 <rnqhsrhkd@gmail.com>
2024-05-27 20:50:26 +0900

    다운로드 링크 수정 및 파일이름 변경

--- imageDownloader.js
+++ imageDownloader.js
@@ -7,11 +7,10 @@ downloadBtn.forEach(button =>{
 function DownloadFile(event)
 {
     const link = document.createElement('a');
-    const newFileName = 'temp.png'
-
+    const newFileName = 'tempDown.png'
 
     // link.href = 'resources/main/introImg.png';
-    link.href = 'https://epi0702.github.io/Ormy_FirstProject/resources/main/introImg.png';
+    link.href = 'https://epi0702.github.io/Ormy_FirstProject/resources/Main/introImg.png';
 
     link.download = newFileName;
 
