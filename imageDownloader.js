fbe5490a3cf021a01a68e3c99027c5e2a2b65d3d
Epi0702 <rnqhsrhkd@gmail.com>
2024-05-27 20:43:34 +0900

    다운로더 경로 변경

--- imageDownloader.js
+++ imageDownloader.js
@@ -9,7 +9,8 @@ function DownloadFile(event)
     const link = document.createElement('a');
     const newFileName = 'temp.png'
 
-    link.href = 'resources/main/introImg.png';
+    // link.href = 'resources/main/introImg.png';
+    link.href = 'https://epi0702.github.io/Ormy_FirstProject/main/introImg.png'
     link.download = newFileName;
 
     document.body.appendChild(link);
